95305725e557a8e8cd7239f6a592a4048babed6d
Tobias Holm <201608355@post.au.dk>
2021-02-19 11:05:14 +0100

    removed web_app

--- web_app/mongoose_lab/routes/students.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var studentController = require('../controllers/student_controller')
-
-
-/* GET add student form */
-router.post('/add', studentController.addStudent);
-
-module.exports = router;
