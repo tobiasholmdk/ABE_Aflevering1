1280bfe1e4cbf77b0f9229f952042df420c0df59
Tobias Holm <201608355@post.au.dk>
2021-03-01 13:04:36 +0100

    Added /addRoom

--- Aflevering1/controllers/hotelcontroller.js
+++ Aflevering1/controllers/hotelcontroller.js
@@ -6,13 +6,30 @@ var room = require('../models/roommodel');
 
 /* POST add hotel form */
 exports.addHotel =  async function (req, res) {
-        var result = await hotel.create({Name: req.body.Name, Address: req.body.Address})
+        
+        const hoteltest = {
+                Name: req.body.Name,
+                Address: req.body.Address,
+                Rooms: {
+                  RoomNumber: req.body.RoomsNumber,
+                  Beds: req.body.Beds
+                }
+              };
+        
+        var result = await hotel.create(hoteltest);
         res.status(200);
         res.json(result);
 };
 
 exports.addRoom =  async function (req, res) {
-        var result = await hotel.findOneAndUpdate(req.body.Name, {Rooms: req.body.Name, Address: req.body.Address});
+
+        var doc = await hotel.findOne({Name: req.body.Name});
+        var newRoom = {
+                RoomNumber: req.body.RoomsNumber,
+                Beds: req.body.Beds
+        }
+        doc.Rooms.push(newRoom);
+        var result = await doc.save();
         res.status(200);
         res.json(result);
 };
