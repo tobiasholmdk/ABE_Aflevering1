b13830a83c14f2c48f8546a55010f4f554fea684
Tobias Holm <201608355@post.au.dk>
2021-03-02 10:05:39 +0100

    Added some swagger docs

--- Aflevering1/routes/hotel.js
+++ Aflevering1/routes/hotel.js
@@ -4,20 +4,93 @@ var hotelcontroller = require("../controllers/hotelcontroller");
 const authorize = require("../_helpers/authorize");
 const roles = require("../_helpers/role");
 
+/* POST add hotel form */
+/**
+ * @swagger
+ * /hotel/addhotel:
+ *   post:
+ *      summary: Manager can add new hotel
+ *      requestBody:
+ *       content:
+ *         application/json:
+ *           schema:      # Request body contents
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *             example:   # Sample object
+ *               name: New Hotel
+ *               address: Hotel Street
+ *      responses:
+ *        200:
+ *          description: OK
+ */
 router.post("/addhotel", authorize(roles.Manager), hotelcontroller.addHotel);
 
+/* POST add hotel form */
+/**
+ * @swagger
+ * /hotel/addroom:
+ *   post:
+ *      summary: Manager can add rooms to a hotel
+ *      requestBody:
+ *       content:
+ *         application/json:
+ *           schema:      # Request body contents
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               roomNumber:
+ *                 type: integer
+ *               beds:
+ *                 type: integer
+ *               reserved:
+ *                 type: boolean
+ *             example:   # Sample object
+ *               name: New Hotel
+ *               roomNumber: 69
+ *               beds: 420
+ *               reserved: false
+ *      responses:
+ *        200:
+ *          description: OK
+ */
 router.post("/addroom", authorize(roles.Manager), hotelcontroller.addRoom);
-router.get("/freerooms", authorize(roles.User), hotelcontroller.getFreeRooms);
 
-/* GET add student form */
+/* GET Free rooms */
+/**
+ * @swagger
+ * /hotel/freerooms:
+ *   post:
+ *      summary: Retrieve the list of vacant rooms
+ *      requestBody:
+ *       content:
+ *         application/json:
+ *           schema:      # Request body contents
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *             example:   # Sample object
+ *               name: Some Hotel name
+ *      responses:
+ *        200:
+ *          description: Retrieve the list of vacant rooms. Can be used something...
+ */
+router.post("/freerooms", authorize(roles.User), hotelcontroller.getFreeRooms);
+
+/* GET add hotels form */
 /**
  * @swagger
  * /hotel/gethotels:
  *   get:
- *      summary: Retrieve the list of students
+ *      summary: Retrieve the list of hotels
  *      responses:
  *        200:
- *          description: Retrieve a list of students. Can be used something...
+ *          description: Retrieve a list of hotels. Can be used something...
  *          content:
  *            application/json:
  *              schema:
@@ -28,3 +101,5 @@ router.get("/freerooms", authorize(roles.User), hotelcontroller.getFreeRooms);
 router.get("/gethotels", authorize(roles.Manager), hotelcontroller.getHotels);
 
 module.exports = router;
+
+
--- Aflevering1/routes/user.js
+++ Aflevering1/routes/user.js
@@ -4,8 +4,99 @@ var usercontroller = require('../controllers/usercontroller');
 const authorize = require('../_helpers/authorize');
 const roles = require('../_helpers/role');
 
+/* POST add hotel form */
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *      summary: User is able to login and get a token
+ *      requestBody:
+ *       content:
+ *         application/json:
+ *           schema:      # Request body contents
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:   # Sample object
+ *               username: My User Name
+ *               password: My Password
+ *      responses:
+ *        200:
+ *          description: OK
+ */
 router.post('/login', usercontroller.login);
+/* POST add hotel form */
+/**
+ * @swagger
+ * /user/adduser:
+ *   post:
+ *      summary: User is able to register
+ *      requestBody:
+ *       content:
+ *         application/json:
+ *           schema:      # Request body contents
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:   # Sample object
+ *               username: My User Name
+ *               password: My Password
+ *      responses:
+ *        200:
+ *          description: OK
+ */
 router.post('/adduser', usercontroller.adduser);
+
+/* POST add hotel form */
+/**
+ * @swagger
+ * /user/changerole:
+ *   post:
+ *      summary: Admin can change role to manager for a user
+ *      requestBody:
+ *       content:
+ *         application/json:
+ *           schema:      # Request body contents
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:   # Sample object
+ *               username: Some User Name
+ *      responses:
+ *        200:
+ *          description: OK
+ */
 router.post('/changerole', authorize(roles.Admin), usercontroller.changeroleManager);
+/* POST add hotel form */
+/**
+ * @swagger
+ * /user/changeroleUser:
+ *   post:
+ *      summary: Admin can change role back to user
+ *      requestBody:
+ *       content:
+ *         application/json:
+ *           schema:      # Request body contents
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:   # Sample object
+ *               username: Some User Name
+ *      responses:
+ *        200:
+ *          description: OK
+ */
 router.post('/changeroleUser', authorize(roles.Admin), usercontroller.changeroleBack);
 module.exports = router;
