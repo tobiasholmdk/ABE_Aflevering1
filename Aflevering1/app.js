bb9c2ce6936e326a79368e90f96baeecf0c76b94
Tobias Holm <201608355@post.au.dk>
2021-03-02 09:48:24 +0100

    Removed old user controllers

--- Aflevering1/app.js
+++ Aflevering1/app.js
@@ -1,40 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors')
-const swaggerJSDoc = require('swagger-jsdoc'); // import swaggerJSDoc from 'swagger-jsdoc';
-const swaggerUi = require('swagger-ui-express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-
-
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const swaggerJSDoc = require("swagger-jsdoc"); // import swaggerJSDoc from 'swagger-jsdoc';
+const swaggerUi = require("swagger-ui-express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+require("./models/db");
+var studentRouter = require("./routes/student");
+var hotelRouter = require("./routes/hotel");
+var userRouter = require("./routes/user");
 
 const swaggerDefinition = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'Express API to manage students',
-version: '1.0.0', },
+    title: "Express API to manage students",
+    version: "1.0.0",
+  },
 };
 const options = {
-swaggerDefinition,
-//Paths to files containing OpenAPI definitions 
-apis: ['./routes/*.js'],
+  swaggerDefinition,
+  //Paths to files containing OpenAPI definitions
+  apis: ["./routes/*.js"],
 };
-
-
-require('./models/db');
-var studentRouter = require('./routes/student');
-var hotelRouter = require('./routes/hotel');
-var userRouter = require('./routes/user')
 
 var app = express();
 const swaggerSpec = swaggerJSDoc(options);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -42,23 +37,19 @@ app.use(bodyParser.json());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-app.use('/student', studentRouter);
-app.use('/hotel', hotelRouter);
-app.use('/user', userRouter);
-
-app.use('/users', require('./users/users.controller'));
+app.use("/student", studentRouter);
+app.use("/hotel", hotelRouter);
+app.use("/user", userRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.json('Error: ' + (err.message || 'Internal server error'));
+  res.json("Error: " + (err.message || "Internal server error"));
 });
 
 module.exports = app;
-
-
